c6e4a25103436e1fe4bd224ff6507cdf30ab204d
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2025-12-09 00:18:11 +0530

    added palindrom no.

---
+++ 08_12_25/palindrom_hw.c
@@ -0,0 +1,26 @@
+// find the palindrome number or not if a user entered number
+#include <stdio.h>
+int main() {
+    int num, reversedNum = 0, originalNum, remainder;
+
+    // Input a number from user
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num; // Store the original number
+
+    // Reverse the number
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    // Check if the original number and reversed number are the same
+    if (originalNum == reversedNum)
+        printf("%d is a palindrome.\n", originalNum);
+    else
+        printf("%d is not a palindrome.\n", originalNum);
+
+    return 0;
+}
