88cc33fab2c55d29c1e0c8cf3e2f07f8398f8c9a
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2025-12-19 12:21:57 +0530

    added lower and upercase

---
+++ 19_12_25/low_up_str.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+// Convert string to uppercase
+void toUpperCase(char str[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A');
+        }
+        i++;
+    }
+}
+
+// Convert string to lowercase
+void toLowerCase(char str[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + ('a' - 'A');
+        }
+        i++;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character
+    str[strcspn(str, "\n")] = '\0';
+
+    toUpperCase(str);
+    printf("Uppercase: %s\n", str);
+
+    toLowerCase(str);
+    printf("Lowercase: %s\n", str);
+
+    return 0;
+}
