b2b37f6c1d4b1a55cf579851c5913b29e7a63aa5
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2025-12-22 12:25:19 +0530

    add all

--- practice/bacis.c
+++ practice/bacis.c
@@ -2,9 +2,13 @@
 
 //the bacis structure of c is
 
-#include <stdio.h>
+#include <stdio.h> 
+//this is a heaeder file which is used to perform input and output operations 
+//e.g printf, scanf etc and it is a standard libarary in c programming
+
 
 int main() {
     printf("hello world\n");
+    return 0; //we use return 0 to indicate that the program ended successfully
 
 }
---
+++ testing.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    
+    int y=2;
+    printf("%d\n",y+=y*=y-=1);
+    return 0;
+}
