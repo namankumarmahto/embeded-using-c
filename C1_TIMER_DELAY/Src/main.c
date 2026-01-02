cb8d863540e12f7d38379fc4eb15535ce311efe0
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2026-01-02 12:12:43 +0530

    added a function GPIO init in the main.c

--- C1_TIMER_DELAY/Src/main.c
+++ C1_TIMER_DELAY/Src/main.c
@@ -9,6 +9,16 @@ void Timer_delay();
 
 int main(void)
 {
+    GPIO_init();
+    Timer_init();
     /* Loop forever */
-	for(;;);
 }
+
+//GPIO Initialization
+void GPIO_init()
+{
+    RCC->AHB1ENR |= (1<<0); //Enable GPIOA clock
+    GPIOA->MODER &= ~(3<<10); //Reset both bits of GPIOA.5
+    GPIOA->MODER |= (1<<10); //Configure GPIOA.5 as output
+}
+
