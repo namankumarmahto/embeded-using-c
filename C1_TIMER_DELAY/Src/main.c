2a2906d713ffcd38bb5069caa1b4ed170d6ba4fa
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2026-01-02 12:15:18 +0530

    added a function timer init in the main.c

--- C1_TIMER_DELAY/Src/main.c
+++ C1_TIMER_DELAY/Src/main.c
@@ -21,4 +21,13 @@ void GPIO_init()
     GPIOA->MODER &= ~(3<<10); //Reset both bits of GPIOA.5
     GPIOA->MODER |= (1<<10); //Configure GPIOA.5 as output
 }
+//Timer Initialization
+void Timer_init()
+{
+    RCC->APB1ENR |= (1<<0); //Enable TIM2 clock
+    TIM2->PSC = 16000; //Frequency scaler
+    TIM2->ARR = 1000-1; //autoreload count
+    TIM2->CNT = 0; // Counting register initialization; //Enable TIM2
+}
 
+//
