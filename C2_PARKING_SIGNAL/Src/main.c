bc5b57a40bae3f10c22bfb56d61fd6ea1f5e05e1
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2026-01-03 11:14:50 +0530

    added address in the main

--- C2_PARKING_SIGNAL/Src/main.c
+++ C2_PARKING_SIGNAL/Src/main.c
@@ -1,5 +1,11 @@
 #include <stdint.h>
 
+#include <stdint.h>
+
+// Base addresses
+#define RCC_Base     0x40023800
+#define GPIOA_Base   0x40020000
+#define GPIOC_Base   0x40020800
 
 
 int main(void)
